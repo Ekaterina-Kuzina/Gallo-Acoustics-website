deec4c257363ba15a6fa5afa3e737e3053d78e1b
Ekaterina <katya.cuzina2011@yandex.ru>
2021-04-03 20:09:23 +0300

    Interactive section is added by js

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@ const panel = document.querySelectorAll('.slider_panel_item');
 const slider = document.querySelector('.slider');
 const panel2 = document.querySelectorAll('.slider_panel_item2');
 const slider2 = document.querySelector('.slider2');
+const listText =document.querySelector('.list_text');
+const listBlock = document.querySelector('.list_block');
+
 
 const obj = {
     img:['img/bg/micro_micro-se.jpeg','img/bg/droplet.jpeg','img/bg/PROFILESUBx2.jpeg'],
@@ -15,9 +18,13 @@ const obj2 = {
     img:['img/bg/micro.jpeg','img/bg/micro-se.jpeg','img/bg/micro-droplet.jpeg','img/bg/strada-2.jpeg','img/bg/PROFILESUBx2.jpeg'],
     span:['ПОБАЛУЙ','ДАВАЙ','ДЕРЖАТЬСЯ'],
     title: ['СЕБЯ ВЫБОРОМ', 'ЗАЖЖЁМ','В ТЕНИ'],
-    text : ['MICRO','MICRO SE', ' MICRO DROPLET', 'MICRO SE DROPLET', 'ADIVA DROPLET', 'ADIVA SE DROPLET','ADIVA','ADIVA SE', 'STRADA 2','SUBWOOFEES']
+    text : ['MICRO','MICRO SE', ' MICRO DROPLET', 'MICRO SE DROPLET', 'ADIVA DROPLET', 'ADIVA SE DROPLET','ADIVA','ADIVA SE', 'STRADA 2','SUBWOOFEES', 'ACCESSORIES']
 };
 
+const obj3 = {
+    img:['img/devices/micro.jpeg','img/devices/micro.jpeg','img/devices/micro_se.jpeg','img/devices/adiva.jpeg', 'img/devices/adiva_se.jpeg','img/devices/micro_droplet.jpeg','img/devices/micro_se_droplet.jpeg','img/devices/adiva_droplet.jpeg','img/devices/adiva_se_droplet.jpeg','img/devices/strada_2.jpeg','img/devices/subwoofers.jpeg', 'img/devices/accessories.jpeg'],
+    text : ['MICRO','MICRO SE','ADIVA','ADIVA SE', ' MICRO DROPLET', 'MICRO SE DROPLET', 'ADIVA DROPLET', 'ADIVA SE DROPLET','STRADA 2','SUBWOOFERS', 'ACCESSORIES']
+};
 
 let index = 0;
 
@@ -103,3 +110,19 @@ function makeContent2(content) {
 }
 makeContent(slider);
 makeContent2(slider2);
+
+
+function addRef(content) {
+    for(let i = 0; i< obj2.text.length; i++){
+        content.innerHTML+= `<a class="list_text_ref" href="#">${obj3.text[i]}${'  '}</a>`;
+    }
+}
+addRef(listText);
+
+document.querySelectorAll('.list_text_ref').forEach((item,i)=>{
+    item.classList.add('mt');
+    item.addEventListener('mouseover', (e)=>{
+        listBlock.innerHTML = '';
+        listBlock.innerHTML = `<img class="list_block_img" src="${obj3.img[i]}" alt="devices">`
+    });
+});
